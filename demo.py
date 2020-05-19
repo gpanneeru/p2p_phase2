4d4975a084a40d98211d6037749b7578042e7d24
gpanneeru <gp1521@nyu.edu>
2020-05-19 17:55:05 -0400

    Working init connect and app_repo

---
+++ demo.py
@@ -0,0 +1,85 @@
+from node import Node
+import time
+import os
+
+class Demo():
+
+    def __init__(self):
+        self.node = None
+
+    def node_callback(self,event, node, connected_node, data):
+        pass
+
+    def connect_with_cached_nodes(self):
+        if not self.node:
+            print("Use init function to first create and initialize a node before connecting to other nodes")
+            return
+        try:
+            f = open("cache","r")
+            for line in f.readlines():
+                host,port = line.split(" ")[0],int(line.split(" ")[1])
+                if not port==self.node.port:
+                    print("Trying to connect with",host+":"+str(port))
+                    self.node.connect_with_node(host,port)
+        except Exception as e:
+            print("Exception:",str(e))
+
+
+    def init(self):
+        try:
+            port = int(input(">Please input the port you want to use: "))
+            self.node = Node("127.0.0.1", port, self.node_callback)
+            self.node.start()
+            time.sleep(1)
+            self.connect_with_cached_nodes()
+            self.node.print_connections()
+            time.sleep(1)
+        except Exception as e:
+            print("Exception:",str(e))
+
+    def show_connections(self):
+        self.node.print_connections()
+
+    def add_repo(self,repo_name):
+        keywords = input("Please enter the keywords related to the repo(comma separated):")
+        keywords = keywords.split(",")
+        if not os.path.exists(self.node.id):
+            os.mkdir(self.node.id)
+        writeto = open(self.node.id+"shared_repo_list",'a+')
+        for keyword in keywords:
+            writeto.write(keyword+" "+repo_name)
+
+    def stop(self):
+        if self.node:
+            self.node.stop()
+    
+def main():
+    print("Welcome to P2P Code Sharing APP")
+    string = ""
+    demo = Demo()
+    while not string=="quit" :
+        string = input("> ")
+        if string=="init":
+            demo.init()
+        elif string=="connect":
+            demo.connect_with_cached_nodes()
+        elif string=="stop":
+            demo.stop()
+        elif string=="help":
+            print("Available Commands: init,connect,show connections,stop,quit")
+        elif string=="quit":
+            print("Quitting the application")
+            demo.stop()
+        elif string=="":
+            pass
+        elif string.startswith("add_repo"):
+            repo_name = string.split(" ")[1]
+            base_path = os.getcwd()
+            demo.add_repo(base_path+"/"+repo_name)
+        elif string=="show connections":
+            demo.show_connections()
+        else:
+            print("Invalid command, use help to see the commands which can be used")
+
+if __name__ == "__main__":
+    main()
